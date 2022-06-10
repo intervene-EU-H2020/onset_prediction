5f0bde2c9271afa7ad4bd6278a98efe737bbbde1
Kira Detrois <kira.detrois@helsinki.fi>
2022-06-10 14:16:40 +0300

    Moved function to own file and documented.

---
+++ Istudysetup/R/create_return_dt.R
@@ -0,0 +1,49 @@
+
+#' Creates a list with relevant information of the selected data
+#' 
+#' Gets only the columns relevant for the current endpoint.
+#' Creates a list with the data, as well as the selected
+#' study period intervals.
+#' 
+#' @inheritParams get_study_elig_indv
+#' 
+#' @return A list(`data`, `exp_age`, `exp_len`, `wash_len`, `out_len`):
+#'         \itemize{
+#'          \item `data`: The actual data.frame.
+#'          \item `exp_age` An integer. Age at which exposure period starts 
+#'                            (in years).
+#'          \item `exp_len` An integer. Length of the exposure period
+#'                               (in years).
+#'          \item `wash_len` An integer. Length of the washout period
+#'                                (in years).
+#'          \item `out_len` An integer. Length of the observation period
+#'                               (in years).
+#'          }
+#' 
+#' @author Kira Detrois
+create_return_dt <- function(pheno_data,
+                             exp_age=30,
+                             exp_len=10,
+                             wash_len=2,
+                             out_len=8,
+                             endpt="J10_ASTHMA") {
+    test_endpt_input_correct(as.list(environment()))
+    test_length_vars_are_integers(as.list(environment()))
+    
+    elig_data <- dplyr::select(pheno_data, 
+                               ID, 
+                               SEX, 
+                               DATE_OF_BIRTH, 
+                               ANCESTRY, 
+                               # Otherwise dplyr will throw error. 
+                               # test_endpt_correct already checks that
+                               # this is only a single string and not
+                               # a vector
+                               dplyr::all_of(endpt), 
+                               paste0(endpt, "_DATE"))
+    elig_data <- list(data=elig_data, 
+                      exp_age=exp_age,
+                      exp_len=exp_len,
+                      wash_len=wash_len,
+                      out_len=out_len)
+}
