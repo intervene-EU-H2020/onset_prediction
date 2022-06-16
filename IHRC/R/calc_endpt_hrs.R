67d677e8f6394eb23945413ce4f1fd3b89d39ad7
Kira Detrois <kira.detrois@helsinki.fi>
2022-06-16 16:13:52 +0300

    Added Documentation

--- IHRC/R/calc_endpt_hrs.R
+++ IHRC/R/calc_endpt_hrs.R
@@ -1,15 +1,56 @@
-#'@export 
+#' Calcualtes HR from a cox-ph model for each endpoint
+#' 
+#' @param pheno_data A data.frame with at least the columns: 
+#'                   `ID`, `SEX`, `DATE_OF_BIRTH`, `ANCESTRY`, 
+#'                   `START_OF_FOLLOWUP`, `END_OF_FOLLOWUP`, 
+#'                   `DATE_OF_BIRTH`, and i.e. `J10_ASTHMA`, and
+#'                   `J10_ASTHMA_DATE` where the columns are the study 
+#'                   endpoint and date, which will differ depending on 
+#'                   the input variable `endpts`.
+#' @param score_data A data.frame with the score results for each individuals.
+#'                   Should have at least column defined in `score_col_name`.
+#' @param score_col_name A character. The name of the column with the scores.
+#' @param score_type A character. The name of the score used for the model,
+#'                      i.e. CCI, or PheRS.
+#' @param endpts A string. The column names of the endpoints of 
+#'                        interest.
+#' @param exp_age An integer. Age at which exposure period starts 
+#'                            (in years).
+#' @param exp_len An integer. Length of the exposure period
+#'                               (in years).
+#' @param wash_len An integer. Length of the washout period
+#'                                (in years).
+#' @param obs_len An integer. Length of the prediction period
+#'                               (in years).
+#' @param downsample_fctr A numeric. Defines how many controls there
+#'                                   should be for every case.
+#'                                   Default is NA, which means no
+#'                                   downsampling is performed.
+#' @param write_hr_res A boolean that defines whether to write the results
+#'                     from the cox-ph model to a file or not. Default is FALSE.
+#' @param write_study_res A boolean that defines whether to write the results from
+#'                        the study selection of eligible individuals for each 
+#'                        endpoint, to files or not. Default is FALSE.
+#' @param write_study_log A character or NA. How to write the log for the study setup.
+#'                        Can be either `NA`: No log. `file`: Write to file, or
+#'                       `print`: Print to console. `file`, and `print` can be used 
+#'                        at the same time.
+#' @param res_dir A character. The directory to write the results to.
+#' 
+#' @export 
+#' 
+#' @author Kira E. Detrois
 calc_endpt_hr <- function(pheno_data, 
                           score_data,
                           score_col_name,
                           score_type,
+                          endpts,
                           exp_age=30,
                           exp_len=10,
                           wash_len=2,
                           obs_len=8,
-                          endpts,
                           downsample_fctr=NA,
-                          write_coxph_res=FALSE,
+                          write_hr_res=FALSE,
                           write_study_res=FALSE,
                           write_study_log=NA,
                           res_dir=NA) {
@@ -48,6 +89,6 @@ calc_endpt_hr <- function(pheno_data,
             message(paste0("Not enough cases for endpoint: ", endpt, " No of cases: ", Istudysetup::get_n_cases(elig_endpt_indv, endpt)))
         }
     }
-    write_coxph_res(as.list(environment()))
+    write_hr_res(as.list(environment()))
     return(all_coxph_res)
 }
--- IHRC/R/run_coxph_ana.R
+++ IHRC/R/run_coxph_ana.R
@@ -1,8 +1,13 @@
+#' Fits the cox-ph model for the current endpoint
+#' 
+#' @param data TODO
+#' @param endpt A string. The column name of the current endpoint of 
+#'                        interest.
 #' @export 
 run_coxph_ana <- function(data,
                           endpt) {
     cox_formula <- stats::as.formula(paste0(
-                    "survival::Surv(", endpt, "_AGE_DAYS,", endpt, ") ~ SCORE"
+                    "survival::Surv(", endpt, "_AGE_DAYS,") ~ SCORE"
                    ))
     coxph_res <- survival::coxph(cox_formula,
                                  data=data)
