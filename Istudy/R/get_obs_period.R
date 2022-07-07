6069dcbe60d37c8d734015806a254424aa155dc9
Kira Detrois <kira.detrois@helsinki.fi>
2022-07-07 17:30:05 +0300

    Added func for observation period

---
+++ Istudy/R/get_obs_period.R
@@ -0,0 +1,15 @@
+#' Calculates the age in the observation period
+#' 
+#' @inheritParams get_study_elig_indv
+#' 
+#' @return A list with the `start` and `end` age in the
+#' observation period
+#' 
+#' @export 
+#' 
+#' @author Kira E. Detrois
+get_obs_period <- function(study) {
+    obs_start <- study@exp_age + study@exp_len + study@wash_len
+    obs_end <- obs_start + study@obs_len
+    return(list(start=obs_start, end=obs_end))
+}
