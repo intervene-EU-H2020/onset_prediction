c7dc4835530bb98a2f22848d20564db8eec6fdbb
Kira Detrois <kira.detrois@helsinki.fi>
2022-08-11 19:59:27 +0300

    Moved ICCI to new repo

--- ICCI/tests/testthat/test-get_comorb_icd_ver_str.R
+++
@@ -1,6 +0,0 @@
-test_that("get_comorb_idc_ver_str works", {
-  expect_equal(get_comorb_icd_ver_str("10"), "charlson_icd10_quan")
-  expect_equal(get_comorb_icd_ver_str("10CM"), "charlson_icd10_quan")
-  expect_equal(get_comorb_icd_ver_str("9"), "charlson_icd9_quan")
-  expect_equal(get_comorb_icd_ver_str("9CM"), "charlson_icd9_quan")
-})
