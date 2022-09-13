9dc2d4bd353c9cd9ddeb4b724da60989d179244c
Kira Detrois <kira.detrois@helsinki.fi>
2022-09-13 15:18:37 +0300

    removed score group stuff

--- IHRC/R/utils_write_files.R
+++ IHRC/R/utils_write_files.R
@@ -2,7 +2,6 @@
 #'  
 #' @inheritParams add_coxph_res_row
 #' @inheritParams add_cidx_res_row
-#' @inheritParams add_risk_group_col
 #' 
 #' @export 
 #' 
@@ -22,40 +21,4 @@ write_res_files <- function(endpt_hrs_tib,
                            file=file_path_cidx,
                            delim="\t")
     }
-}
-
-#' Write score cut-off values and groups to log
-#' 
-#' @inheritParams get_risk_group_labs
-#' @inheritParams add_risk_group_col
-#' 
-#' @export 
-#' 
-#' @author Kira E. Detrois
-write_score_groups_to_log <- function(score_group_tbl,
-                                      surv_ana) {
-    file_path <- check_and_get_file_path(surv_ana=surv_ana,
-                                         res_type="log")
-    if(!is.null(file_path)) {
-        readr::write_delim(log_msg_table(score_group_tbl), 
-                           file=file_path, 
-                           delim=" ",
-                           col_names=TRUE)
-    }
-}
-
-#' Creates a string of the current study setup
-#' 
-#' @inheritParams get_risk_group_labs
-#' 
-#' @return A tibble with columns `GROUP`, `SCORE_CUT`
-#' 
-#' @export 
-#' 
-#' @author Kira E. Detrois
-log_msg_table <- function(score_group_tbl) {
-    score_tib <- tibble::tibble(GROUP=names(score_group_tbl), 
-                                SCORE_CUT=score_group_tbl)
-
-    return(score_tib)
 }
