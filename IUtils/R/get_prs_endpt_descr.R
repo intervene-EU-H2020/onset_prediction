3e7583f27335f15f7db9a9fc596aacbc36535314
Kira Detrois <kira.detrois@helsinki.fi>
2023-07-04 13:45:39 +0300

    Updated default endpoints

--- IUtils/R/get_prs_endpt_descr.R
+++ IUtils/R/get_prs_endpt_descr.R
@@ -8,6 +8,6 @@ get_prs_endpt_descr <- function() {
     "Breast_Cancer", "CHD", "Colorectal_Cancer", "Epilepsy", 
     "Gout", "Hip_Osteoarthritis", "Knee_Osteoarthritis", "MDD", "Melanoma", 
     "Prostate_Cancer", "Rheumatoid_Arthritis", "T2D", "ILD", "Lung_Cancer", 
-    "Focal_Epilepsy", "Generalised_Epilepsy", "G6_AD_WIDE", "Sleep_Apnoea", "AAA", 
-    "I9_HEARTFAIL_NS", "Inflammatory_Bowel_Disease", "N14_CHRONKIDNEYDIS", "Osteoporosis")
+    "G6_AD_WIDE", "Sleep_Apnoea", "Inflammatory_Bowel_Disease", 
+    "N14_CHRONKIDNEYDIS", "I9_HEARTFAIL_NS")
 }
