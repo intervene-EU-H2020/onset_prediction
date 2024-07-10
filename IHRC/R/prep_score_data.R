a22cf61edcba823c4fee7d46abc0aa6c278bf0f1
Kira <kira.detrois@helsinki.fi>
2024-07-10 14:13:46 +0300

    added regressing out age and sex and grouping for predictors

--- IHRC/R/prep_score_data.R
+++ IHRC/R/prep_score_data.R
@@ -29,26 +29,36 @@ preprocess_score_data <- function(score_type,
                                   write_progress=FALSE) {
     
     score_data = NULL
-    score_data <- add_prs_endpt_data(score_data=score_data,
-                                     score_type=score_type,
-                                     prs_data=prs_data,
-                                     endpt=endpt)
+
+    age_temp <- dplyr::select(pheno_data, ID, SEX, DATE_OF_BIRTH, matches("PC"))
+    age_temp$YEAR_OF_BIRTH <- lubridate::year(age_temp$DATE_OF_BIRTH)
+    age_temp$SEX <- as.factor(age_temp$SEX)
+    score_data <- dplyr::select(age_temp, ID, SEX, YEAR_OF_BIRTH, matches("PC"))
+
     score_data <- add_phers_endpt_data(score_data=score_data,
                                        score_type=score_type,
                                        phers_data=phers_data,
                                        endpt=endpt)
+    score_data <- add_prs_endpt_data(score_data=score_data,
+                                     score_type=score_type,
+                                     prs_data=prs_data,
+                                     endpt=endpt)
     score_data <- add_edu_cont_data(score_data=score_data,
                                     score_type=score_type,
                                     pheno_data=pheno_data)
-    score_data <- add_prob_data(score_data=score_data,
-                                    score_type=score_type,
-                                    pheno_data=pheno_data)
+    score_data <- add_cci_data(score_data=score_data,
+                               score_type=score_type,
+                               pheno_data=pheno_data,
+                               icd_data=icd_data,
+                               study_setup=study_setup)
 
-    if(!all(score_type[!(score_type %in% c("BMI", "EDU", "ZIP", "CCI", "SMOKING"))] %in% colnames(score_data))) {
-        missing_score <- score_type[!(score_type %in% c("EDU", "ZIP", "BMI", "CCI", "SMOKING")) & !(score_type %in% colnames(score_data))]
+    if(!all(score_type[!(score_type %in% c("BMI", "EDU_UKBB", "EDUCATION_11", "EDUCATION_97", "EDU", "ZIP", "SMOKING"))] %in% colnames(score_data))) {
+        missing_score <- score_type[!(score_type %in% c("EDU_UKBB", "EDUCATION_11","EDUCATION_97", "EDU",  "ZIP", "BMI", "SMOKING")) & !(score_type %in% colnames(score_data))]
         write_to_error_file(error_file, paste0("Something went wrong when getting the score data for endpoint ", endpt, ". Missing ", paste0(missing_score, collapse=", "), " data.\n"))
         score_data <- NULL
     }
+
+    score_data <- dplyr::select(score_data, -SEX, -YEAR_OF_BIRTH, -matches("PC"))
     return(score_data)
 }
 
@@ -81,21 +91,25 @@ add_cci_data <- function(score_data,
         if(!is.null(new_score_data)) {
             score_data <- new_score_data
         } 
-    }     
-    if("EI" %in% score_type) {
-        ei_data <- get_study_cci_data(pheno_data,
-                                      icd_data,
-                                      score_type="CCI",
-                                      study_setup)  
-        if(!is.null(score_data) & !is.null(ei_data)) {
-            new_score_data <- dplyr::full_join(score_data, ei_data, by="ID")
+        score_data <- dplyr::filter(score_data, !is.na(CCI), !is.na(PheRS), !is.na(YEAR_OF_BIRTH), !is.na(PC1), !is.na(PC2), !is.na(PC3), !is.na(PC4), !is.na(PC5), !is.na(PC6), !is.na(PC7), !is.na(PC8), !is.na(PC9), !is.na(PC10), !is.na(SEX))
+
+        score_data$CCI[is.na(score_data$CCI)] <- 0
+
+        score_data$CCI_orig <- score_data$CCI
+
+        if(length(unique(score_data$SEX)) > 1) {
+            score_data$CCI <- scale(residuals(glm(scale(CCI_orig)~scale(YEAR_OF_BIRTH)+SEX+scale(PC1)+scale(PC2)+scale(PC3)+scale(PC4)+scale(PC5)+scale(PC6)+scale(PC7)+scale(PC8)+scale(PC9)+scale(PC10), data=score_data)))[,1]
         } else {
-            new_score_data <- ei_data
+            score_data$CCI <- scale(residuals(glm(scale(CCI_orig)~scale(YEAR_OF_BIRTH)+scale(PC1)+scale(PC2)+scale(PC3)+scale(PC4)+scale(PC5)+scale(PC6)+scale(PC7)+scale(PC8)+scale(PC9)+scale(PC10), data=score_data)))[,1]
         }
-        if(!is.null(new_score_data)) {
-            score_data <- new_score_data
-        }
-    }
+
+        #Assign PRS into percentiles
+        q <- quantile(score_data$CCI, probs=c(0,0.9,1), na.rm=TRUE)
+
+        score_data["CCI_group"] <- cut(score_data$CCI, q, include.lowest=TRUE, labels=c("0", "1"))
+        #score_data["CCI_group"] <- ifelse(score_data$CCI < 2, "0", "1")
+    }     
+
     return(score_data)
 }
 
@@ -114,7 +128,6 @@ add_prs_endpt_data <- function(score_data,
                                prs_data,
                                endpt) {
     if("PRS" %in% score_type) {
-        
         PRS_data <- get_prs_endpt_data(score_data=prs_data,
                                        endpt=endpt)
         if(!is.null(score_data) & !is.null(PRS_data)) {
@@ -125,7 +138,23 @@ add_prs_endpt_data <- function(score_data,
         if(!is.null(new_score_data)) {
             score_data <- new_score_data
         } 
+        score_data <- dplyr::filter(score_data, !is.na(PRS), !is.na(PheRS), !is.na(YEAR_OF_BIRTH), !is.na(PC1), !is.na(PC2), !is.na(PC3), !is.na(PC4), !is.na(PC5), !is.na(PC6), !is.na(PC7), !is.na(PC8), !is.na(PC9), !is.na(PC10), !is.na(SEX))
+
+        score_data$PRS_orig <- score_data$PRS
+
+        if(length(unique(score_data$SEX)) > 1) {
+            score_data$PRS <- scale(residuals(glm(scale(PRS_orig)~scale(YEAR_OF_BIRTH)+SEX+scale(PC1)+scale(PC2)+scale(PC3)+scale(PC4)+scale(PC5)+scale(PC6)+scale(PC7)+scale(PC8)+scale(PC9)+scale(PC10), data=score_data)))[,1]
+        } else {
+            score_data$PRS <- scale(residuals(glm(scale(PRS_orig)~scale(YEAR_OF_BIRTH)+scale(PC1)+scale(PC2)+scale(PC3)+scale(PC4)+scale(PC5)+scale(PC6)+scale(PC7)+scale(PC8)+scale(PC9)+scale(PC10), data=score_data)))[,1]
+        }
+
+        #Assign PRS into percentiles
+        p<-c(0,0.1,0.2,0.4)
+        q <- quantile(score_data$PRS, probs=c(p,rev(1-p)), na.rm=TRUE)
+
+        score_data["PRS_group"] <- cut(score_data$PRS, q, include.lowest=TRUE, labels=paste(1:(2*length(p)-1)))
     }
+
     return(score_data)
 }
 
@@ -156,6 +185,20 @@ add_phers_endpt_data  <- function(score_data,
         if(!is.null(new_score_data)) {
             score_data <- new_score_data
         } 
+        score_data <- dplyr::filter(score_data, !is.na(PheRS), !is.na(YEAR_OF_BIRTH), !is.na(PC1), !is.na(PC2), !is.na(PC3), !is.na(PC4), !is.na(PC5), !is.na(PC6), !is.na(PC7), !is.na(PC8), !is.na(PC9), !is.na(PC10), !is.na(SEX))
+        score_data$PheRS_orig <- score_data$PheRS
+
+        if(length(unique(score_data$SEX)) > 1) {
+            score_data$PheRS <- scale(residuals(glm(scale(PheRS_orig)~scale(YEAR_OF_BIRTH)+SEX+scale(PC1)+scale(PC2)+scale(PC3)+scale(PC4)+scale(PC5)+scale(PC6)+scale(PC7)+scale(PC8)+scale(PC9)+scale(PC10), data=score_data)))[,1]
+        } else {
+            score_data$PheRS <- scale(residuals(glm(scale(PheRS_orig)~scale(YEAR_OF_BIRTH)+scale(PC1)+scale(PC2)+scale(PC3)+scale(PC4)+scale(PC5)+scale(PC6)+scale(PC7)+scale(PC8)+scale(PC9)+scale(PC10), data=score_data)))[,1]
+        }
+
+        #Assign PheRS into percentiles  
+        p<-c(0,0.1,0.2,0.4)
+        q <- quantile(score_data$PheRS, probs=c(p,rev(1-p)), na.rm=TRUE)
+
+        score_data["PheRS_group"] <- cut(score_data$PheRS, q, include.lowest=TRUE, labels=paste(1:(2*length(p)-1)))
     }
 
     # Repeat for transfer
@@ -172,38 +215,16 @@ add_phers_endpt_data  <- function(score_data,
         }
         if(!is.null(new_score_data)) {
             score_data <- new_score_data
-        } 
-    }
-    print(score_data)
-    return(score_data)
-}
-
-#' Adds the medication score endpoint data to the score data
-#' 
-#' @param score_data A data.frame. The current score data for the study setup.
-#' @inheritParams preprocess_score_data
-#' 
-#' @return A tibble. The score data with added column `MED`.
-#' 
-#' @author Kira E. Detrois
-#' 
-#' @export 
-add_med_endpt_data <- function(score_data,
-                               score_type,
-                               pheno_data,
-                               study_setup,
-                               atc_data) {
-    if("MED" %in% score_type) {
-        med_data <- get_study_med_data(pheno_data, study_setup, atc_data)
-        if(!is.null(score_data) & !is.null(med_data)) {
-            new_score_data <- dplyr::full_join(score_data, med_data, by="ID")
-        } else {
-            new_score_data <- med_data
         }
-        if(!is.null(new_score_data)) {
-            score_data <- new_score_data
-        } 
+
+        score_data <- dplyr::filter(score_data, !is.na(PheRS_transfer))
+        #Assign PheRS into percentiles  
+        p<-c(0,0.1,0.2,0.4)
+        q <- quantile(score_data$PheRS_transfer, probs=c(p,rev(1-p)), na.rm=TRUE)
+
+        score_data["PheRS_transfer_group"] <- cut(score_data$PheRS_transfer, q, include.lowest=TRUE, labels=paste(1:(2*length(p)-1)))
     }
+ 
     return(score_data)
 }
 
@@ -235,6 +256,19 @@ add_edu_cont_data <- function(score_data,
         if(!is.null(new_score_data)) {
             score_data <- new_score_data
         } 
+        score_data <- dplyr::filter(score_data, !is.na(EDU_cont), !is.na(PheRS), !is.na(YEAR_OF_BIRTH), !is.na(SEX))
+
+        score_data$EDU_cont_orig <- score_data$EDU_cont
+        if(length(unique(score_data$SEX)) > 1) {
+            score_data$EDU_cont <- scale(residuals(glm(scale(EDU_cont_orig)~scale(YEAR_OF_BIRTH)+SEX+scale(PC1)+scale(PC2)+scale(PC3)+scale(PC4)+scale(PC5)+scale(PC6)+scale(PC7)+scale(PC8)+scale(PC9)+scale(PC10), data=score_data)))[,1]
+        } else {
+            score_data$EDU_cont <- scale(residuals(glm(scale(EDU_cont_orig)~scale(YEAR_OF_BIRTH)+scale(PC1)+scale(PC2)+scale(PC3)+scale(PC4)+scale(PC5)+scale(PC6)+scale(PC7)+scale(PC8)+scale(PC9)+scale(PC10), data=score_data)))[,1]
+        }
+
+        #Assign Education into percentiles
+        q <- quantile(score_data$EDU_cont, probs=c(0,0.6,1), na.rm=TRUE)
+
+        score_data["EDU_cont_group"] <- cut(score_data$EDU_cont, q, include.lowest=TRUE, labels=c("0", "1"))
     }
     return(score_data)
 }
